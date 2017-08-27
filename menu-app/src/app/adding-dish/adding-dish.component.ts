9d5585530b9062cfbfb82f42de5e381aa7bbd256
Victoria Gerasymchuk <victoria.gerasymchuk@gmail.com>
2017-08-27 20:32:39 +0300

    added saving dish to localStorage

--- menu-app/src/app/adding-dish/adding-dish.component.ts
+++ menu-app/src/app/adding-dish/adding-dish.component.ts
@@ -21,4 +21,32 @@ export class AddingDishComponent implements OnInit {
   ngOnInit() {
   }
 
+  addDish() {
+	var menu = [];
+	// Get menu from Local Storage
+	var m = localStorage.getItem("DISH_MENU");
+
+	// Menu should be not null. If it doesn't than crate array else parse json string
+	if (m != null) {
+		menu = JSON.parse(m);
+	}
+
+	// Add new dish to menu
+	menu.push(this.dish);
+
+	// Save new menu to Local Storage
+	localStorage.setItem("DISH_MENU", JSON.stringify(menu));
+
+	alert("Saved!");
+
+	// Clear data on form
+	this.dish = {
+		name: '',
+		description: '',
+		price: 0,
+		image: '',
+		dateFrom: '',
+		dateTo: ''
+	};
+  }
 }
