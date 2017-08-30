2cd4e2caeba401ba17e5620f1e7ef022b1894e0e
Victoria Gerasymchuk <victoria.gerasymchuk@gmail.com>
2017-08-30 23:03:16 +0300

    added routing

--- menu-app/src/app/app.module.ts
+++ menu-app/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { AddingDishComponent } from './adding-dish/adding-dish.component';
 import { MenuListComponent } from './menu-list/menu-list.component';
 import { EditingDishComponent } from './editing-dish/editing-dish.component';
+
+// определение маршрутов
+const appRoutes: Routes = [
+    { path: '', component: MenuListComponent },
+    { path: 'add', component: AddingDishComponent },
+    { path: 'edit', component: EditingDishComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { EditingDishComponent } from './editing-dish/editing-dish.component';
   ],
   imports: [
     BrowserModule,
-	FormsModule 
+	FormsModule,
+	RouterModule.forRoot(appRoutes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
